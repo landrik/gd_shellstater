910bffb1d3874808f590d2174ae7a853a9c2cdc5
Landry Karege <Landry.Karege@avalon.net>
2018-01-22 19:03:26 +0000

    first commit

---
+++ app/assets/js/scripts.js
@@ -0,0 +1,16 @@
+/*!
+ * GlobalData
+ * a GlobalData blueprint for products front-ends
+ * https://globaldata.com/fastshell
+ * @author Landry Karege
+ * @version 1.0.0
+ * Copyright 2018. MIT licensed.
+ */
+(function ($, window, document, undefined) {
+  'use strict';
+  $(function () {
+    
+
+  });
+
+})(jQuery, window, document);
